23df0f26e7cf6ab595f41e68d4ffe94839d45156
Nick Jackson <admin@nljackson.dev>
2020-09-23 11:26:57 -0500

    Creating a separate Test Database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyB_88DAH7keedgEELqCqo4NzMrRDl_MZv0",
-  authDomain: "expensify-23b26.firebaseapp.com",
-  databaseURL: "https://expensify-23b26.firebaseio.com",
-  projectId: "expensify-23b26",
-  storageBucket: "expensify-23b26.appspot.com",
-  messagingSenderId: "196933200423",
-  appId: "1:196933200423:web:f64e7aaa5ef20499c13c02",
-  measurementId: "G-NN2C2N8Z4H"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STOREAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
 firebase.initializeApp(config)
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,3 +1,4 @@
+import DotEnv from 'dotenv'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
@@ -5,3 +6,4 @@ Enzyme.configure({
   adapter: new Adapter()
 })
 
+DotEnv.config({ path: '.env.test' })
