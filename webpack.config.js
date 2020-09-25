cae9931ded4eb0773fc6b15a23281ce2a415fe44
Nick Jackson <admin@nljackson.dev>
2020-09-25 14:55:03 -0500

    Removed babel polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = (env) => {
   const isProduction = env === 'production'
 
   return {
-    entry: ['babel-polyfill', './src/app.js'],
+    entry: './src/app.js',
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
